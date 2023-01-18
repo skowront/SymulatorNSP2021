caf9f6695e653fe4670a208df3cda0ef653cfaf1
skowront <tomasz.skowron98@gmail.com>
2023-01-18 20:07:34 +0000

    Deploying to gh-pages from @ skowront/SymulatorNSP2021@42eebe45308ce77956ed15aed049ca9d65b09274 🚀

---
+++ _content/SaveFile.js
@@ -0,0 +1,9 @@
+﻿function saveFile(file, Content) {
+    var link = document.createElement('a');
+    link.download = file;
+    console.log(link.download);
+    link.href = "data:text/plain;charset=utf-8," + encodeURIComponent(Content)
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+}
